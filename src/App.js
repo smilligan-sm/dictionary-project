56bf0c05d921ecbe09993a6753780683ea636d48
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-26 09:52:23 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./milligantechlogosmall.png";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello There!</h1>
+        <img src={logo} className="App-logo" alt="logo"/>
+      </header>
+    </div>
+  );
+}
+
