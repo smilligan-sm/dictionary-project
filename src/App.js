d73f9aa6ab6805b94cc0699a3bed26c5f7b0dc4f
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-26 16:02:53 +1000

    Added search engine component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./milligantechlogosmall.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello There!</h1>
-        <img src={logo} className="App-logo" alt="logo"/>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello There!</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Sarah Milligan</footer>
+      </div>
     </div>
   );
 }
